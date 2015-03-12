dc93220376f9880a897f047fe678b086b8030ff3
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-12 22:18:48 +0300

    Answers Controller Refactoring

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,12 @@
 class AnswersController < ApplicationController
   before_action :get_question
+  before_action :load_answer, only: [:show, :edit, :update, :destroy]
 
   def index
     @answers = @question.answers
   end
 
   def show
-    @answer = @question.answers.find(params[:id])
   end
 
   def new
@@ -14,7 +14,6 @@ class AnswersController < ApplicationController
   end
 
   def edit
-    @answer = @question.answers.find(params[:id])
   end
 
   def create
@@ -27,7 +26,6 @@ class AnswersController < ApplicationController
   end
 
   def update
-    @answer = @question.answers.find(params[:id])
     if @answer.update(answer_params)
       redirect_to [@question, @answer]
     else
@@ -46,6 +44,10 @@ class AnswersController < ApplicationController
   def get_question 
     @question = Question.find(params[:question_id])
   end
+
+  def load_answer
+    @answer = @question.answers.find(params[:id])
+  end  
 
   def answer_params
     params.require(:answer).permit(:body)
