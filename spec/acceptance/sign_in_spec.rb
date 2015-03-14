3f44593bec65fee9bfa5f1b96febccf7e7377e5b
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-14 19:41:07 +0300

    Add acceptance tests: user sign out, guest sign up.

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -24,5 +24,47 @@ feature 'User sign in', %q{
     expect(page).to have_content 'Invalid email or password.'
     expect(current_path).to eq new_user_session_path
   end
+end
 
+feature 'User sign out', %q{
+  After working with site user wants to be logged out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Signed in user try to sign out.' do
+    sign_in(user)
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+end
+
+feature 'Guest sign up', %q{
+  In order to be able to ask questions
+  Guest wants to sign up
+} do
+
+  scenario 'Guest try to sign up with write details' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Guest try to sign up with write details' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: nil
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'error'
+    expect(current_path).to eq user_registration_path
+  end
 end
