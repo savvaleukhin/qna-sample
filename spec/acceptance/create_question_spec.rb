4e1639aee78501af33d230d11efd124f4767c169
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-14 18:04:37 +0300

    Acceptance tests refactoring.

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -6,13 +6,10 @@ feature 'Create question', %q{
   I want to be able to ask a question
 }do
 
-  scenario 'Authenticated user creates a question' do
-    User.create!(email: 'user@test.com', password: '12345678')
+  given(:user) { create(:user) }
 
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-    click_on 'Log in'
+  scenario 'Authenticated user creates a question' do
+    sign_in(user)
 
     visit questions_path
     click_on 'Ask question'
