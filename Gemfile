92975442bb2771a9f50a59ad7d217dd9647a992d
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-14 12:47:39 +0300

    Add User model. Add devise. Add test for signing in.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'slim-rails'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -51,4 +52,5 @@ end
 group :test do
   gem 'shoulda-matchers'
   gem 'capybara'
+  gem 'launchy'
 end
--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -12,7 +12,7 @@ feature 'User sign in', %q{
     visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sign_in'
+    click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
     expect(current_path).to eq root_path
@@ -22,7 +22,7 @@ feature 'User sign in', %q{
     visit new_user_session_path
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sign_in'
+    click_on 'Log in'
 
     expect(page).to have_content 'Invalid email or password.'
     expect(current_path).to eq new_user_session_path
