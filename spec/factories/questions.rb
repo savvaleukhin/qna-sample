087515184bd2793603a4b2d8134a07e40048e57f
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-15 18:34:13 +0300

    Add show question with answers

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,10 +2,27 @@ FactoryGirl.define do
   factory :question do
     title "MyString"
     body "MyText"
+
+    factory :question_2 do
+      title "Second question"
+    end  
   end
 
   factory :invalid_question, class: "Question" do
     title nil
     body nil
   end
+
+  factory :question_with_answers, class: "Question" do
+    title "Question with answers"
+    body "Text of question with answers"
+
+    transient do
+      answers_count 2
+    end
+
+    after(:create) do |question, evaluator|
+      create_list(:answer_list, evaluator.answers_count, question: question)
+    end      
+  end    
 end
