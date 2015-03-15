63663d442cb65400ecbf2a0adedd8c9f770494b6
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-15 18:34:13 +0300

    Add show question with answers

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
+  sequence :body do |n|
+    "Answer \##{n}"
+  end
+
   factory :answer do
     body "MyText"
+  end
+
+  factory :answer_list, class: "Answer" do
+    body
+    question
   end
 
   factory :invalid_answer, class: "Answer" do
