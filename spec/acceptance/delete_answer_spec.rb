3ddecbd81b98675f9e8ab6b4fe1f07c179c17de7
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-18 20:20:49 +0300

    Add acceptance test delete answers.

--- spec/acceptance/delete_answer_spec.rb
+++ spec/acceptance/delete_answer_spec.rb
@@ -8,25 +8,21 @@ feature 'Delete an anwer', %q{
   given(:user_non_owner) { create(:user) }
 
   scenario 'Non-authenticated user tries to delete an answer' do
-    page.driver.submit :delete, question_answer_path(answer_with_user.question, answer_with_user), {}
-
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
-    expect(current_path).to eq new_user_session_path
+    visit question_answer_path(answer_with_user.question, answer_with_user)
+    expect(page).to_not have_selector(:link_or_button, 'Delete')
   end
 
   scenario 'Authenticated user (not the owner) tries to delete an answer' do
     sign_in(user_non_owner)
+    visit question_answer_path(answer_with_user.question, answer_with_user)
 
-    page.driver.submit :delete, question_answer_path(answer_with_user.question, answer_with_user), {}
-
-    expect(page).to have_content 'You do not have permission to view this page.'
-    expect(current_path).to eq root_path
+    expect(page).to_not have_selector(:link_or_button, 'Delete')
   end
 
   scenario 'Authenticated user (owner) delete an answer' do
     sign_in(answer_with_user.user)
-
-    page.driver.submit :delete, question_answer_path(answer_with_user.question, answer_with_user), {}
+    visit question_answer_path(answer_with_user.question, answer_with_user)
+    click_on 'Delete'
 
     expect(page).to have_content 'Your answer was successfully deleted.'
     expect(current_path).to eq question_answers_path(answer_with_user.question)
