ff8e5dd2527c7f7311196bf03d80849ac01e2ae2
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-27 20:37:19 +0300

    Polymorphic attachments. Answers associated with attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,11 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :user_id, :body, presence: true
+  accepts_nested_attributes_for :attachments
+
   scope :by_top, -> { order('accepted DESC') }
 
   def accept
