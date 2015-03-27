ffda4fe44a282d216cae41599e6d2c23f7c6f1d3
saveliy <zlinilla@yandex.ru>
2015-03-27 16:20:04 +0300

    Refactoring filename delegate

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 class FileUploader < CarrierWave::Uploader::Base
+  delegate :filename, to: :file
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
