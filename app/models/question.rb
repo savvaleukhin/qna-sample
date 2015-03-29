da5ae5be6bc4391e6e4046411752b03a2310fd34
Saveliy Leukhin <zlinilla@gmail.com>
2015-03-29 21:29:04 +0300

    Add several files to answer and question

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,5 +6,5 @@ class Question < ActiveRecord::Base
   validates :user_id, :title, :body, presence: true
   validates :title, length: { maximum: 100 }
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: lambda { |a| a[:file].blank? }
 end
--- spec/acceptance/add_files_to_answers_spec.rb
+++ spec/acceptance/add_files_to_answers_spec.rb
@@ -23,4 +23,19 @@ feature 'Add files to answers', %q{
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
     end
   end
+
+  scenario 'User adds several files when he is creating an answer', js: true do
+    click_on 'New attachment'
+    fields = page.all('input[type="file"]')
+
+    fill_in 'Your answer', with: 'My answer'
+    fields[0].set "#{Rails.root}/spec/spec_helper.rb"
+    fields[1].set "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'Create'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
+  end
 end
