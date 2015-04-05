50f15f037dd5863b66c860b6d18ff8a2f0e6c8e1
Saveliy Leukhin <zlinilla@gmail.com>
2015-04-05 18:04:55 +0300

    Add model tests for voting system

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -15,4 +15,57 @@ RSpec.describe Vote, type: :model do
 
     should validate_uniqueness_of(:user_id).scoped_to([:votable_id, :votable_type])
   end
+
+  describe 'Vote for question' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+
+    it 'votes up' do
+      vote = question.vote(user, 1)
+
+      expect(vote.value).to eq 1
+      expect(Vote.where(user_id: user, votable_id: question.id, votable_type: question.class.name).count).to eq 1
+    end
+
+    it 'votes down' do
+      vote = question.vote(user, -1)
+
+      expect(vote.value).to eq -1
+      expect(Vote.where(user_id: user, votable_id: question.id, votable_type: question.class.name).count).to eq 1
+    end
+
+    it 'unvotes' do
+      question.vote(user, 1)
+      unvote = question.unvote(user)
+
+      expect(Vote.where(user_id: user, votable_id: question.id, votable_type: question.class.name).count).to eq 0
+    end
+  end
+
+  describe 'Vote for answer' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:answer) { create(:answer, question: question, user: user) }
+
+    it 'votes up' do
+      vote = answer.vote(user, 1)
+
+      expect(vote.value).to eq 1
+      expect(Vote.where(user_id: user, votable_id: answer.id, votable_type: answer.class.name).count).to eq 1
+    end
+
+    it 'votes down' do
+      vote = answer.vote(user, -1)
+
+      expect(vote.value).to eq -1
+      expect(Vote.where(user_id: user, votable_id: answer.id, votable_type: answer.class.name).count).to eq 1
+    end
+
+    it 'unvotes' do
+      answer.vote(user, 1)
+      unvote = answer.unvote(user)
+
+      expect(Vote.where(user_id: user, votable_id: answer.id, votable_type: answer.class.name).count).to eq 0
+    end
+  end
 end
