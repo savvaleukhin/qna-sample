72d96173b4dd6507c32eacf3f739190694ccd0be
saveliy <zlinilla@yandex.ru>
2015-04-06 15:09:07 +0300

    Change vote uniq validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
 
   validates :user_id, :votable_id, :votable_type, :value, presence: true
-  validates_uniqueness_of :user_id, scope: [:votable_id, :votable_type]
+  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
   validates_inclusion_of :value, in: [1, -1]
 end
