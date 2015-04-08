07963a9b2db2afb48db1766257ba650d7c1f0f02
saveliy <zlinilla@yandex.ru>
2015-04-08 12:51:07 +0300

    Optimization for Votable. Replace checking voted_by before delete_all

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -6,18 +6,17 @@ module Votable
   end
 
   def vote(user, value)
-    if self.voted_by?(user)
-      Vote.transaction do
-        Vote.where(votable_id: self.id, votable_type: self.class.name, user_id: user.id).delete_all
-        Vote.create(votable_id: self.id, votable_type: self.class.name, user_id: user.id, value: value)
-      end
-    else
-      Vote.create(votable_id: self.id, votable_type: self.class.name, user_id: user.id, value: value)
+    Vote.transaction do
+      Vote.where(votable_id: id, votable_type: self.class.name, user_id: user.id).delete_all
+      Vote.create(votable_id: id,
+                  votable_type: self.class.name,
+                  user_id: user.id,
+                  value: value)
     end
   end
 
   def unvote(user)
-    user.votes.where(votable: self).delete_all if self.voted_by?(user)
+    user.votes.where(votable: self).delete_all
   end
 
   def voted_by?(user)
@@ -25,11 +24,11 @@ module Votable
   end
 
   def total_votes
-    Vote.where(votable_id: self.id, votable_type: self.class.name).sum(:value)
+    Vote.where(votable_id: id, votable_type: self.class.name).sum(:value)
   end
 
   def vote_by(user)
-    vote = Vote.find_by(votable_id: self.id, votable_type: self.class.name)
+    vote = Vote.find_by(votable_id: id, votable_type: self.class.name, user_id: user.id)
     vote.value unless vote.nil?
   end
 end
