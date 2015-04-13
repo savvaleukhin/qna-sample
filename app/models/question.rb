402271e7d5cf60d787df17d750ce4279a6cf880a
saveliy <zlinilla@yandex.ru>
2015-04-13 16:42:06 +0300

    Add association for comments to user and question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,10 @@
 class Question < ActiveRecord::Base
   include Votable
 
+  belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachmentable, dependent: :destroy
-  belongs_to :user
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :user_id, :title, :body, presence: true
   validates :title, length: { maximum: 100 }
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:attachments).dependent(:destroy) }
+  it { should have_many(:votes).dependent(:destroy) }
+  it { should have_many(:comments).dependent(:destroy) }
+
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should validate_length_of(:title).is_at_most(100) }
-  it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:attachments).dependent(:destroy) }
-  it { should have_many(:votes).dependent(:destroy) }
+
   it { should accept_nested_attributes_for :attachments }
 end
