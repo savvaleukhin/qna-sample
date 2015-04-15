0ce20fc3765fa5a32699617d9c77d2cf42bab3d4
saveliy <zlinilla@yandex.ru>
2015-04-15 15:32:58 +0300

    Add model test for answer has many comments

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :body }
   it { should belong_to :question }
   it { should have_many(:attachments).dependent(:destroy) }
-  it { should accept_nested_attributes_for(:attachments) }
   it { should have_many(:votes).dependent(:destroy) }
+  it { should have_many(:comments).dependent(:destroy) }
+
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :body }
+  it { should accept_nested_attributes_for(:attachments) }
+
 
   describe 'Accept answer' do
     let(:user) { create(:user) }
