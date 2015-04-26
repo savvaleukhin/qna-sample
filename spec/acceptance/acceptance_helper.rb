ae08bb191a401a33ff8813fb082f642478cfeba0
Saveliy Leukhin <zlinilla@gmail.com>
2015-04-26 16:59:13 +0300

    Rename acceptance helper module to feature helper.

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -3,7 +3,7 @@ require'rails_helper'
 RSpec.configure do |config|
   Capybara.javascript_driver = :webkit
 
-  config.include AcceptanceHelper, type: :feature
+  config.include FeatureHelper, type: :feature
 
   config.use_transactional_fixtures = false
 
