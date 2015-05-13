4739bc8d006bc98f48fa127501d1dbdd80cbe2be
Saveliy Leukhin <zlinilla@gmail.com>
2015-05-13 21:16:23 +0300

    Change parent classes for serializers

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,5 +1,4 @@
-class AnswerSerializer < ActiveModel::Serializer
-  attributes :id, :body, :created_at, :updated_at, :user_id, :accepted
+class AnswerSerializer < AnswerPreviewSerializer
   has_many :comments
   has_many :attachments
 end
