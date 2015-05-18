f60c70c37e41ce92d53a81ff347ddd96774b0523
saveliy <zlinilla@yandex.ru>
2015-05-18 16:25:06 +0300

    Remove redundant method update_reputation

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,12 +10,4 @@ class Question < ActiveRecord::Base
   validates :title, length: { maximum: 100 }
 
   accepts_nested_attributes_for :attachments, reject_if: -> (a) { a[:file].blank? }, allow_destroy: true
-
-  private
-
-  def update_reputation(method)
-    diff = Reputation.calculate(self, method)
-    reputation = self.user.reputation + diff
-    self.user.update(reputation: reputation)
-  end
 end
