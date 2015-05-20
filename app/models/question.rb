2fc43380fb949ab16242419c0263aee2b3434d27
saveliy <zlinilla@yandex.ru>
2015-05-20 10:27:40 +0300

    Fix subscription association mistake. Move it from Answer to Question.

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,8 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachmentable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :subscriptions, foreign_key: "tracking_question_id", dependent: :destroy
+  has_many :subscribers, through: :subscriptions
 
   validates :user_id, :title, :body, presence: true
   validates :title, length: { maximum: 100 }
