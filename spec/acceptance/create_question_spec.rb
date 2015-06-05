f65a60de6ac984bde997c75835e4f37993ae6af6
saveliy <zlinilla@yandex.ru>
2015-06-05 16:42:37 +0300

    Style/Upgrade new question form

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -8,17 +8,38 @@ feature 'Create question', %q{
 
   given(:user) { create(:user) }
 
-  scenario 'Authenticated user creates a question' do
-    sign_in(user)
+  context 'Authenticated user' do
+    scenario 'Authenticated user creates a question' do
+      sign_in(user)
 
-    visit questions_path
-    click_on 'Ask question'
-    fill_in 'Title', with: 'My new question'
-    fill_in 'Body', with: 'test text'
-    click_on 'Save'
+      visit questions_path
+      click_on 'Ask question'
+      fill_in 'Title', with: 'My new question'
+      fill_in 'Body', with: 'test text'
+      click_on 'Save'
 
-    expect(page).to have_content 'Question was successfully created.'
-    expect(page).to have_content 'My new question'
+      expect(page).to have_content 'Question was successfully created.'
+      expect(page).to have_content 'My new question'
+    end
+
+    scenario 'Authenticated user cancels creating process' do
+      sign_in(user)
+      visit questions_path
+      click_on 'Ask question'
+      click_on 'Cancel'
+
+      expect(current_path).to eq questions_path
+    end
+
+    scenario 'Tries to create invalid question' do
+      sign_in(user)
+
+      visit new_question_path
+      click_on 'Save'
+
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Body can't be blank "
+    end
   end
 
   scenario 'Non-authenticated user tries to create a question' do
